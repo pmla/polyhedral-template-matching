38c822b99ccc224d192e84d391f996f001729969
Jakob Schiotz <schiotz@fysik.dtu.dk>
2016-03-11 11:32:40 +0100

    Wrote a setup.py that works on Linux.  It works if Python is compiled with g++.

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
-raise RuntimeError("THIS setup.py FILE HAS NOT BEEN FINISHED: Please type make to build instead.")
+# NOTE: This setup.py script requires that Python is compiled with a
+# C++ compiler, and that this compiler supports C++11.  This is only
+# occationally the case!
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 from glob import glob
+import os
 
 c_src_files = glob('*.c')
-cpp_src_files = glob('*.cpp')
+cpp_src_files = glob('*.cpp') + glob('svdpolar/*.cpp')
 extra_compile_args = ['-std=c++11']
 
-name = 'polyhedraltemplatematching'
+name = 'ptmmodule'
 
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(os.path.join(numpy.get_include(), 'numpy'))
 
 
 setup(name=name,
@@ -20,4 +31,6 @@ setup(name=name,
                              extra_compile_args=extra_compile_args,
                              )
                   ],
+      setup_requires=['numpy'],
+      cmdclass={'build_ext':build_ext},
       )
